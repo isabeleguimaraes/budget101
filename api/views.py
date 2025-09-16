0f1534f50a0ad0d67592e4702b6dc44b0b5c7f9f
isabeleguimaraes <belepfg@gmail.com>
2025-09-15 23:51:21 -0700

    feat(views) Add user filters to querysets and associate user on create

--- api/views.py
+++ api/views.py
@@ -5,27 +5,48 @@ from rest_framework.permissions import IsAuthenticated
 
 # GET and POST a Category
 class CategoryListCreate(generics.ListCreateAPIView):
-    queryset = Category.objects.all()
+
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 # GET, DELETE, UPDATE a Category by ID
 class CategoryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Category.objects.all()
+
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
+
+
 # GET and CREATE an Expense
 class ExpenseListCreate(generics.ListCreateAPIView):
-    queryset = Expense.objects.all()
+   
     serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
 #GET, UPDATE and DELETE an Expense by ID
 class ExpenseRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Expense.objects.all()
+    
     serializer_class = ExpenseSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Expense.objects.filter(user=self.request.user)
+
 
 # POST a user (Sign Up Authentication Process by Django)
 class SignUpView(generics.CreateAPIView):
